0815b628de70aac47465bdbbc9fcb1d2aee18ca7
samumirandam <samuelchampi@gmail.com>
2020-11-21 15:16:04 -0500

    Se organiza la carpeta public

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/calcularMedidas.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public/js"),
     filename: "calcularMedidas-dist.js",
   },
   resolve: {
